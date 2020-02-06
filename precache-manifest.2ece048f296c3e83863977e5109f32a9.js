335ac53090849a5fb0f3dc994e69306547560327
Alireza Tehrani <s.alireza.t@gmail.com>
2020-02-06 14:45:54 +0330

    Updates

---
+++ precache-manifest.2ece048f296c3e83863977e5109f32a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c220cd0b7ccd9e6e6167984426455db3",
+    "url": "/resume-app/index.html"
+  },
+  {
+    "revision": "6ca1b963c5f812669611",
+    "url": "/resume-app/static/css/main.5f9aa534.chunk.css"
+  },
+  {
+    "revision": "75ab2b3201790f22c4c1",
+    "url": "/resume-app/static/js/2.637d78de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/resume-app/static/js/2.637d78de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca1b963c5f812669611",
+    "url": "/resume-app/static/js/main.1f2e8262.chunk.js"
+  },
+  {
+    "revision": "76c5861b9fa6880da79c",
+    "url": "/resume-app/static/js/runtime-main.93eda473.js"
+  }
+]);
